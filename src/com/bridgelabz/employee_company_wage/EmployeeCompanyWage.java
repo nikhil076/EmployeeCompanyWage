9ab7146db9386b9911f1975a20fecc9a0037d7d5
nikhil076 <nikhil.arvind7@gmail.com>
2021-06-28 14:56:01 +0530

    Added condition to check employee wage till 100 max hours or 20 days

--- src/com/bridgelabz/employee_company_wage/EmployeeCompanyWage.java
+++ src/com/bridgelabz/employee_company_wage/EmployeeCompanyWage.java
@@ -3,33 +3,41 @@ package com.bridgelabz.employee_company_wage;
 public class EmployeeCompanyWage {
 	public static final int IS_FULLTIME_PRESENT  = 1;
 	public static final int IS_PARTTIME_PRESENT = 2;
+	public static final int MAX_HOURS_ALLOWED = 100;
+	public static final int DAYS_IN_A_MONTH = 20;
 	public static final int WAGE_PER_DAY = 20;
-	public static final int FULL_DAY = 8;
-	public static final int PART_TIME = 4;
-	public static final int WORKING_DAYS_PER_MONTH = 20;
 
 	public static void main(String[] args) {
 		System.out.println("Welcome to Employee Company Wage");
-		int MONTHLY_WAGE = 0;
+		int totalWorkingHours = 0;
+		int totalWorkingDays = 0;
+		int monthlyWage = 0;
 		
-		for (int i=0; i<WORKING_DAYS_PER_MONTH; i++)
-		{
-			int DAILY_WAGE = 0;
-			double empCheck = Math.floor(Math.random() * 10 ) % 3;
+		while(totalWorkingHours<MAX_HOURS_ALLOWED && totalWorkingDays<DAYS_IN_A_MONTH){
 			
+			int dailyWage = 0;
+			totalWorkingDays++;
+			int employeeHours=0;
+			
+			double empCheck = Math.floor(Math.random() * 10 ) % 3;			
 			switch ((int)empCheck) {
 			case IS_FULLTIME_PRESENT : 
-				DAILY_WAGE = WAGE_PER_DAY * FULL_DAY;
+				employeeHours = 8;
 				break;
 				
 			case IS_PARTTIME_PRESENT :
-				DAILY_WAGE = WAGE_PER_DAY * PART_TIME;
+				employeeHours = 4;
 				break;
 			
 			default:
+				employeeHours = 0;
 			}
-			MONTHLY_WAGE = MONTHLY_WAGE + DAILY_WAGE;
+			dailyWage = WAGE_PER_DAY * employeeHours;
+			totalWorkingHours += employeeHours;
+			monthlyWage += dailyWage;
 		}
-		System.out.println("Monthly employee wage :"+MONTHLY_WAGE);
+		System.out.println("Total Working Days :"+totalWorkingDays);
+		System.out.println("Total Wokking hours :"+totalWorkingHours);
+		System.out.println("Monthly employee wage :"+monthlyWage);
 	}
 }
