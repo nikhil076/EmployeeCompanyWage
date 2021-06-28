da4a2d6e3fde69d24eaa7c0670656757bc256159
nikhil076 <nikhil.arvind7@gmail.com>
2021-06-28 14:25:17 +0530

    inititalized the program

---
+++ src/com/bridgelabz/employee_company_wage/EmployeeCompanyWage.java
@@ -0,0 +1,8 @@
+package com.bridgelabz.employee_company_wage;
+
+public class EmployeeCompanyWage {
+
+	public static void main(String[] args) {
+		System.out.println("Welcome to Employee Company Wage");
+	}
+}
