d329754a95cdabf749824ab8feb990b25e1a9e8c
nikhil076 <nikhil.arvind7@gmail.com>
2021-06-28 14:27:44 +0530

    Added the functionality to check employee attendance

--- src/com/bridgelabz/employee_company_wage/EmployeeCompanyWage.java
+++ src/com/bridgelabz/employee_company_wage/EmployeeCompanyWage.java
@@ -1,8 +1,19 @@
 package com.bridgelabz.employee_company_wage;
 
 public class EmployeeCompanyWage {
+	public static final int IS_PRESENT = 1;
 
 	public static void main(String[] args) {
 		System.out.println("Welcome to Employee Company Wage");
+		double empCheck = Math.floor(Math.random() * 10 ) % 2;
+		
+		if (empCheck == IS_PRESENT)
+		{
+			System.out.println("Employee is present");
+		}
+		else
+		{
+			System.out.println("Employee is absent");
+		}
 	}
 }
